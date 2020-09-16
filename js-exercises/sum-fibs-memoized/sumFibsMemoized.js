53cda48b1e9f60bd826e1ae1455c2b77f4b36c5d
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2020-09-16 18:34:53 +0530

    test

--- js-exercises/sum-fibs-memoized/sumFibsMemoized.js
+++ js-exercises/sum-fibs-memoized/sumFibsMemoized.js
@@ -1,17 +1,5 @@
 function sumFibs(num) {
-  var start = 1;
-  var next = 1;
-  var current;
-  var sumOdd = 0;
-  for (var i = 0; i <= num; i++){
-  current = start + next;
-  start = next;
-  next = current;
-  if (current % 2 !== 0 && current <= num){
-  sumOdd = current + sumOdd; 
-  }
-  }
-  return sumOdd + 2;//accounting for the first two 1's
+
   }
 
 function cacheFunction() {}
