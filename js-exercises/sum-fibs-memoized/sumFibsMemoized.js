ffef0d92f7642fe33761bfbd67ec648cb86fbf84
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2020-09-17 17:14:11 +0530

    solved sumFibsMemoized

--- js-exercises/sum-fibs-memoized/sumFibsMemoized.js
+++ js-exercises/sum-fibs-memoized/sumFibsMemoized.js
@@ -3,29 +3,32 @@ function sumFibs(num) {
   let q = 1;
   let newVal;
   let sum = 0;
-  for(let i=0; i<=num;++i){
+  for (let i = 0; i <= num; ++i) {
     newVal = p + q;
     p = q;
     q = newVal;
-    if(newVal%2!=0 && newVal<=num){
-      sum+=newVal;
+    if (newVal % 2 != 0 && newVal <= num) {
+      sum += newVal;
     }
 
   }
-  return sum+2; //accounting for the first two 1's
-  }
+
+  return sum + 2; //accounting for the first two 1's
+}
+
+let cache = {};
 
 function cacheFunction(n) {
-  let cache = {};
-  if(cache[n]){
+  if (cache[n]) {
     return cache[n];
   }
-  else{
-    cache[n]=sumFibs(n);
+  else {
+    cache[n] = sumFibs(n);
     return cache[n]
   }
 
 }
+
 
 
 export { sumFibs, cacheFunction };
